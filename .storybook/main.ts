bc2d4ea24743f49f8950b760b8de31d3113b4791
OHS <ohs6006@gmail.com>
2024-05-15 00:44:46 +0900

    CI CD UI TEst

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,11 +8,13 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+
   docs: {
     autodocs: "tag",
   },
